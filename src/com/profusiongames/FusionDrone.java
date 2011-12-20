ab80af063cecee21eef25b847033a4bdcafba44a
MORP Project <swtfhs1@gmail.com>
2011-12-20 14:34:50 +0100

    Reduced Number of Permissions

--- src/com/profusiongames/FusionDrone.java
+++ src/com/profusiongames/FusionDrone.java
@@ -12,7 +12,6 @@ import android.hardware.SensorManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
@@ -21,8 +20,6 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.codeminders.ardrone.ARDrone;
 import com.codeminders.ardrone.DroneVideoListener;
 import com.codeminders.ardrone.NavData;
