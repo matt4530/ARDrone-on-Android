943e233fb497dad304d7f9c18e38aad268f7ae7a
MORP Project <swtfhs1@gmail.com>
2011-12-21 13:50:28 +0100

    Removed unused permissions and streamlined layout
    Signed-off-by: MORP Project <swtfhs1@gmail.com>

--- gen/com/profusiongames/R.java
+++ gen/com/profusiongames/R.java
@@ -25,19 +25,18 @@ public final class R {
         public static final int buttonBGLayout=0x7f060003;
         public static final int connectButton=0x7f060004;
         public static final int droneVideoDisplay=0x7f060002;
-        public static final int dualjoystickView=0x7f060013;
-        public static final int heightText=0x7f060011;
+        public static final int dualjoystickView=0x7f060012;
+        public static final int heightText=0x7f060010;
         public static final int launchButton=0x7f06000a;
         public static final int linearLayout1=0x7f06000d;
-        public static final int linearLayout2=0x7f06000e;
-        public static final int linearLayout3=0x7f06000f;
-        public static final int linearLayout4=0x7f060012;
+        public static final int linearLayout3=0x7f06000e;
+        public static final int linearLayout4=0x7f060011;
         public static final int mayday=0x7f060006;
         public static final int progressBar1=0x7f060009;
         public static final int relativeLayout1=0x7f060001;
         public static final int relativeLayout2=0x7f060008;
         public static final int spinner1=0x7f060007;
-        public static final int spinner2=0x7f060010;
+        public static final int spinner2=0x7f06000f;
         public static final int statusBar=0x7f060000;
     }
     public static final class layout {
